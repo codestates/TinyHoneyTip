5a7d1d5f436570ddc1ec77439acaed19b015083f
SangminSuk <tjrtkdals96@gmail.com>
2021-09-09 10:10:37 +0900

    [server]/router

---
+++ server/controllers/Index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mypage: require('./mypage/Index'),
+    user: require('./user/Index'),
+    post: require('./post/Index'),
+    signin: async (req, res) => {},
+    signup: async (req, res) => {},
+    signout: async (req, res) => {},
+};

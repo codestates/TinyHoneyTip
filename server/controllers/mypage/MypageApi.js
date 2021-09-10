b1fbec46e861afcdd3d2ede3b0f13509322fae86
peb <dmdqld24@gmail.com>
2021-09-10 15:33:10 +0900

    Feat: Add mypage API, skeleton

--- server/controllers/mypage/MypageApi.js
+++ server/controllers/mypage/MypageApi.js
@@ -1,8 +1,67 @@
-const { user, post_contailner } = require('../../models');
+const { user, post_container, scrap } = require('../../models');
 const jwt = require('jsonwebtoken');
+const { isAuthorized } = require('../tokenFunctions');
 require('dotenv').config();
 
 module.exports = {
-    getmypage,
-    editmypage,
+    getmypage: async (req, res) => {
+        const accessToken = req.cookies.accessToken;
+        try {
+            if (!accessToken) res.status(404).json({ message: 'Bad Request' });
+            else {
+                const token = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
+                if (!token) res.status(404).json({ message: 'Bad Request' });
+                else {
+                    const findUserInfo = await user.findAll({
+                        where: { userId: token },
+                    });
+                    const findMyPost = await post_container.findAll({
+                        where: { userId: token },
+                    });
+                    const findMyScrap = await scrap.findAll({
+                        where: { userId: token },
+                    });
+                    res.status(200).json({
+                        message: 'ok',
+                        data: {
+                            userInfo: findUserInfo,
+                            mypost: findMyPost,
+                            myscrap: findMyScrap,
+                            alert: '',
+                        },
+                    });
+                }
+            }
+        } catch (err) {
+            res.status(400).json({ message: 'Bad Request' });
+        }
+    },
+    editmypage: async (req, res) => {
+        const accessToken = req.cookies.accessToken;
+        try {
+            if (!accessToken) res.status(404).json({ message: 'Bad Request' });
+            else {
+                const token = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
+                if (!token) res.status(404).json({ message: 'Bad Request' });
+                else {
+                    const { email, username, password, profile_img } = req.params;
+                    const newInfo = await user.update({
+                        where: { id: token },
+                        email,
+                        username,
+                        password,
+                        profile_img,
+                    });
+                    res.status(200).json({
+                        message: 'ok',
+                        data: {
+                            userInfo: newInfo,
+                        },
+                    });
+                }
+            }
+        } catch (err) {
+            res.status(400).json({ message: 'Bad Request' });
+        }
+    },
 };

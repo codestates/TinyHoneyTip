3ec6d7bdee5a61fa42dcd92d795fa596cb0172d8
wktaylorla <wktaylorla@gmail.com>
2021-09-17 15:22:05 +0900

    fix error, fix file structure

--- client/pages/post/[id].js
+++ client/pages/post/[id].js
@@ -2,8 +2,7 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Post from '../../src/components/Post';
-
-export default function Id() {
+export default function Id({ userInfo }) {
     const router = useRouter();
     const { id } = router.query;
 
@@ -12,9 +11,25 @@ export default function Id() {
     const API_URL = `${process.env.NEXT_PUBLIC_URL}/post/${id}`;
 
     const getPostsData = () => {
-        axios.get(API_URL).then((res) => {
-            setPost(res.data.data.post);
-        });
+        console.log('페이지 렌더링');
+        console.log(userInfo);
+        axios
+            .get(API_URL, {
+                headers: {
+                    // 'content-Type': 'application/json',
+                    cookie: userInfo.accessToken,
+                    // 'accept-encoding': 'gzip, deflate, br',
+                    // Connection: 'keep-alive',
+                },
+                withCredentials: true,
+            })
+            .then((res) => {
+                setPost(res.data.data.post);
+            })
+            .catch((error) => {
+                console.log('page 정보 불러오기 실패!');
+                console.log(error);
+            });
     };
 
     // useEffect(() => {
@@ -29,5 +44,11 @@ export default function Id() {
         }
     }, [id]);
 
-    return <Post post={post} />;
+    useEffect(() => {
+        if (id && id > 0) {
+            getPostsData();
+        }
+    }, [userInfo]);
+
+    return <Post userInfo={userInfo} post={post} />;
 }

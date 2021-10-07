02b2a49b1ddbb5defdccd5a0b72d2c44b24c3703
SangminSuk <tjrtkdals96@gmail.com>
2021-10-07 13:43:17 +0900

    mmerge

--- server/controllers/Index.js
+++ server/controllers/Index.js
@@ -31,7 +31,7 @@ module.exports = {
             } else if (!findemail && !finduser) {
                 res.status(400).json({ message: 'rewrite email' });
             } else {
-                const accessToken = jwt.sign(finduser.dataValues, process.env.ACCESS_SECRET);
+                const accessToken = jwt.sign(finduser.dataValues, process.env.ACCESS_SECRET, { expiresIn: '2s' });
 
                 res.cookie('accessToken', accessToken, {
                     sameSite: 'none',
